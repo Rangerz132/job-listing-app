77a732b52147273a3f52312a8f575d04cc1447c8
Anthony Trépanier <38358928+Rangerz132@users.noreply.github.com>
2025-02-21 15:39:26 -0500

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Header from "./components/header";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,27 @@
+import HeaderMobile from "../assets/images/bg-header-mobile.svg";
+import HeaderDesktop from "../assets/images/bg-header-desktop.svg";
+
+const Header = () => {
+  return (
+    <div className="w-full h-20 bg-desaturated-dark-cyan">
+      {/** Mobile header */}
+      <div
+        className="w-full h-full md:hidden"
+        style={{
+          backgroundImage: `url("${HeaderMobile}")`,
+          backgroundSize: "cover",
+        }}
+      ></div>
+      {/** Desktop header */}
+      <div
+        className="w-full h-full hidden md:flex"
+        style={{
+          backgroundImage: `url("${HeaderDesktop}")`,
+          backgroundSize: "cover",
+        }}
+      ></div>
+    </div>
+  );
+};
+
+export default Header;
